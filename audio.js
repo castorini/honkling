0c6ce559bdfc638c5591ac577a324a6f7535a599
Brandon Lee <ljj7975@hotmail.com>
2018-08-02 17:20:43 -0400

    supporting 2d data print

--- audio.js
+++ audio.js
@@ -218,11 +218,27 @@ class Audio {
 
   printData(name, data) {
     console.log(name, data)
-    const arrMin = arr => Math.min(...arr);
-    const arrMax = arr => Math.max(...arr);
-    const arrAvg = arr => arr.reduce((a,b) => a + b, 0) / arr.length
 
-    console.log('\trange : ( ', arrMin(data), ' ~ ', arrMax(data), ' )');
-    console.log('\tmean : ', arrAvg(data))
+    if (data.length == 0) {
+      console.log('\tlength is zero');
+    } else {
+      if (Array.isArray(data[0])) {
+        // 2D array
+        var temp = data;
+        data = [];
+
+        for (var i = 0; i < temp.length; i++) {
+          for (var j = 0; j < temp[i].length; j++) {
+            data.push(temp[i][j]);
+          }
+        }
+      }
+      const arrMin = arr => Math.min(...arr);
+      const arrMax = arr => Math.max(...arr);
+      const arrAvg = arr => arr.reduce((a,b) => a + b, 0) / arr.length
+
+      console.log('\trange : ( ', arrMin(data), ' ~ ', arrMax(data), ' )');
+      console.log('\tmean : ', arrAvg(data))
+    }
   }
 }
