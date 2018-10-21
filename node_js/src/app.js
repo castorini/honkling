642c69f6360528f4bd194a9259b4bdc2847075b1
Brandon Lee <j474lee@edu.uwaterloo.ca>
2018-10-20 21:51:49 -0400

    warming up node js application before evaluation

--- node_js/src/app.js
+++ node_js/src/app.js
@@ -26,7 +26,7 @@ function initEval(valSize, testSize) {
   console.log('validation set size : ', valSize);
   console.log('test set size : ', testSize);
   console.log('===========================================\n');
-  
+
   evaluate('val', valSize)(function() {
     console.log('< validation dataset evaluation completed >');
     displayResult('val');
@@ -48,16 +48,43 @@ function evaluate(type, size) {
   return evaluator.collectMeasurement();
 }
 
-request(encodeData(serverURL+'/init', {
-  commands : commands.toString(),
-  randomSeed :10,
-  sampleRate : audioConfig['offlineSampleRate'],
-  appId : appId
-}), function (error, response, body) {
-  if (error != null) {
-    console.log('server initialization has failed :', error);
-    return;
-  }
-  let parsed = JSON.parse(body);
-  initEval(parsed["valCount"], parsed["testCount"]);
+let warmUpDeferred;
+function warmUp() {
+  warmUpDeferred = deferred();
+  request(encodeData(serverURL+'/get_audio', {
+    index : 0,
+    type : 'test',
+    appId : 0
+  }), function (error, response, body) {
+    if (error != null) {
+      let msg = 'retrieving audio data for warm up failed : ' + error;
+      warmUpDeferred.reject(msg);
+      return;
+    }
+    let audio = JSON.parse(body);
+    for (var i = 0; i < 5; i++) {
+      let mfcc = transposeFlatten2d(getMFCC(audio['features']));
+      model.predict(mfcc);
+    }
+    warmUpDeferred.resolve();
+  });
+  return warmUpDeferred.promise;
+}
+
+warmUp()(function() {
+  request(encodeData(serverURL+'/init', {
+    commands : commands.toString(),
+    randomSeed :10,
+    sampleRate : audioConfig['offlineSampleRate'],
+    appId : appId
+  }), function (error, response, body) {
+    if (error != null) {
+      console.log('server initialization has failed :', error);
+      return;
+    }
+    let parsed = JSON.parse(body);
+    initEval(parsed["valCount"], parsed["testCount"]);
+  });
+},function(msg) {
+  console.log('warming up failed : ' + msg);
 });
