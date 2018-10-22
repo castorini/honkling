8e711de2bb1c576ced858a852b872f06765372e7
Brandon Lee <j474lee@edu.uwaterloo.ca>
2018-10-22 13:03:44 -0400

    evaluation of all every model with node

--- node_js/src/app.js
+++ node_js/src/app.js
@@ -4,8 +4,8 @@ var Model = require('./model.js');
 var deferred = require('deferred');
 var request = require('request');
 
-let appId = new Date().getTime();
-let model = new Model();
+let appId = 0;
+let model = new Model("RES8_NARROW"); // for warm up
 
 function displayResult(type) {
   request(encodeData(serverURL+'/get_report', {
@@ -14,77 +14,140 @@ function displayResult(type) {
   }), function (error, response, body) {
     if (error != null) {
       console.log('retrieval of report has failed :', error);
+
+      evalCompleteDeferred.reject(error);
       return;
     }
     let parsed = JSON.parse(body);
     console.log(parsed);
-    console.log('===========================================\n');
+    console.log('==================\n');
+
+
+    if (parsed.type == 'test') {
+      evalCompleteDeferred.resolve();
+    }
   });
 }
 
-function initEval(valSize, testSize) {
+let serverInitDeferred;
+function initServer() {
+  serverInitDeferred = deferred();
+  request(encodeData(serverURL+'/init', {
+    commands : commands.toString(),
+    sampleRate : audioConfig['offlineSampleRate'],
+    appId : appId
+  }), function (error, response, body) {
+    if (error != null) {
+      let msg = 'server initialization has failed :' + error;
+      serverInitDeferred.rejected(msg);
+      return;
+    }
+    let parsed = JSON.parse(body);
+    serverInitDeferred.resolve(parsed);
+  });
+  return serverInitDeferred.promise;
+}
+
+let evalCompleteDeferred;
+function evalModel(modelName) {
+  evalCompleteDeferred = deferred();
+  model = new Model(modelName);
+
+  console.log('\n\n\n\n\n====== evaluation of model ' + modelName + ', ID : ' + appId + ' ======\n');
+
+  initServer()(function(initSummary) {
+    evaluate(initSummary["valTotal"], initSummary["testTotal"])()(function () {
+    evalCompleteDeferred.resolve();
+    }, function(err) {
+      console.log('evaluation model '+modelNames[modelIndex]+' failed', err);
+      evalCompleteDeferred.reject(err);
+    })
+  }, function(err) {
+    evalCompleteDeferred.reject(err);
+  })
+  return evalCompleteDeferred.promise;
+}
+
+function collectData(type, size) {
+  let evaluator = new Evaluator(appId, model, type, size);
+  return evaluator.collectMeasurement();
+}
+
+function evaluate(valSize, testSize) {
   console.log('validation set size : ', valSize);
   console.log('test set size : ', testSize);
   console.log('===========================================\n');
 
-  evaluate('val', valSize)(function() {
-    console.log('< validation dataset evaluation completed >');
+  collectData('val', valSize)(function() {
+    console.log('//// validation dataset evaluation completed ////\n');
     displayResult('val');
 
-    evaluate('test', testSize)(function() {
-      console.log('< test dataset evaluation completed >');
+    collectData('test', testSize)(function() {
+      console.log('//// test dataset evaluation completed ////\n');
       displayResult('test');
     },function(msg) {
-      console.log('< test dataset evaluation failed >');
+      console.log('//// test dataset evaluation failed ////\n');
+      evalCompleteDeferred.reject(msg);
     });
 
   },function(msg) {
-    console.log('< val dataset evaluation failed >');
+    console.log('//// val dataset evaluation failed ////\n');
+    evalCompleteDeferred.reject(msg);
   });
 }
 
-function evaluate(type, size) {
-  let evaluator = new Evaluator(appId, model, type, size);
-  return evaluator.collectMeasurement();
+let modelIndex = 0;
+let overAllEvaluationDeferred = deferred();
+function evalAllModels() {
+  appId = new Date().getTime();
+  evalModel(modelNames[modelIndex])(function() {
+    modelIndex++;
+    if (modelIndex < modelNames.length) {
+      evalAllModels();
+    } else {
+      overAllEvaluationDeferred.resolve();
+    }
+  }, function(err) {
+    console.log('model eval failed');
+    overAllEvaluationDeferred.reject();
+  });
+  return overAllEvaluationDeferred.promise;
 }
 
 let warmUpDeferred;
 function warmUp() {
   warmUpDeferred = deferred();
-  request(encodeData(serverURL+'/get_audio', {
-    index : 0,
-    type : 'test',
-    appId : 0
-  }), function (error, response, body) {
-    if (error != null) {
-      let msg = 'retrieving audio data for warm up failed : ' + error;
-      warmUpDeferred.reject(msg);
-      return;
-    }
-    let audio = JSON.parse(body);
-    for (var i = 0; i < 5; i++) {
-      let mfcc = transposeFlatten2d(getMFCC(audio['features']));
-      model.predict(mfcc);
-    }
-    warmUpDeferred.resolve();
-  });
+  initServer()(function() {
+    request(encodeData(serverURL+'/get_audio', {
+      index : 0,
+      type : 'test',
+      appId : appId
+    }), function (error, response, body) {
+      if (error != null) {
+        let msg = 'retrieving audio data for warm up failed : ' + error;
+        warmUpDeferred.reject(msg);
+        return;
+      }
+      let audio = JSON.parse(body);
+      for (var i = 0; i < 5; i++) {
+        let mfcc = transposeFlatten2d(getMFCC(audio['features']));
+        model.predict(mfcc);
+      }
+      warmUpDeferred.resolve();
+    });
+  }, function(err) {
+    let msg = 'init server for warm up failed : ' + error;
+    warmUpDeferred.reject(msg);
+  })
   return warmUpDeferred.promise;
 }
 
 warmUp()(function() {
-  request(encodeData(serverURL+'/init', {
-    commands : commands.toString(),
-    randomSeed :10,
-    sampleRate : audioConfig['offlineSampleRate'],
-    appId : appId
-  }), function (error, response, body) {
-    if (error != null) {
-      console.log('server initialization has failed :', error);
-      return;
-    }
-    let parsed = JSON.parse(body);
-    initEval(parsed["valCount"], parsed["testCount"]);
+  console.log('warm up is completed');
+  evalAllModels()(function() {
+    console.log('evaluation is completed');
   });
+
 },function(msg) {
   console.log('warming up failed : ' + msg);
 });
