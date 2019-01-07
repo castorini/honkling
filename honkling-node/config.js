14ae53be170717cdbfc172df168fbf77296dd0d7
Brandon Lee <ljj7975@gmail.com>
2019-01-07 18:12:40 -0500

    including honkling node (#86)

---
+++ honkling-node/config.js
@@ -0,0 +1,75 @@
+// valid model names are :
+// 	"RES8", "RES8_40", "RES8_80", "RES8_NARROW", "RES8_NARROW_40", "RES8_NARROW_80"
+exports.modelName = "RES8_NARROW";
+
+exports.commands = ["silence", "unknown", "yes", "no", "up", "down", "left", "right", "on", "off", "stop", "go"];
+exports.mojibarCommands = ["silence", "unknown", "workplace", "volume", "up", "down", "wikipedia", "canada", "list", "one", "two", "three", "four", "right", "left", "open", "top", "memory"];
+exports.lightCommands = ["silence", "unknown", "on", "off", "up", "down"];
+
+exports.predictionFrequency = 350; // ms between mic audio prediction
+exports.predictionThreshold = 0.85;
+exports.noiseThreshold = 0.015;
+exports.sampleRate = 16000;
+exports.meydaBufferSize = 512;
+exports.meydaWindowSlidingSize = 10; // ms
+
+exports.modelConfigs = {
+	RES8 : {
+		input_shape : [40, 101, 1],
+		n_layers : 6,
+		n_feature_maps : 45,
+		res_pool : [3, 4],
+		conv_size : [3, 3],
+		conv_stride : [1, 1],
+		use_dilation : false
+	},
+	RES8_40 : {
+		input_shape : [40, 101, 1],
+		n_layers : 6,
+		n_feature_maps : 45,
+		n_kept_feature : 27,
+		res_pool : [3, 4],
+		conv_size : [3, 3],
+		conv_stride : [1, 1],
+		use_dilation : false
+	},
+	RES8_80 : {
+		input_shape : [40, 101, 1],
+		n_layers : 6,
+		n_feature_maps : 45,
+		n_kept_feature : 9,
+		res_pool : [3, 4],
+		conv_size : [3, 3],
+		conv_stride : [1, 1],
+		use_dilation : false
+	},
+	RES8_NARROW : {
+		input_shape : [40, 101, 1],
+		n_layers : 6,
+		n_feature_maps : 19,
+		res_pool : [3, 4],
+		use_dilation : false,
+		conv_size : [3, 3],
+		conv_stride : [1, 1]
+	},
+	RES8_NARROW_40 : {
+		input_shape : [40, 101, 1],
+		n_layers : 6,
+		n_feature_maps : 19,
+		n_kept_feature : 12,
+		res_pool : [3, 4],
+		use_dilation : false,
+		conv_size : [3, 3],
+		conv_stride : [1, 1]
+	},
+	RES8_NARROW_80 : {
+		input_shape : [40, 101, 1],
+		n_layers : 6,
+		n_feature_maps : 19,
+		n_kept_feature : 4,
+		res_pool : [3, 4],
+		use_dilation : false,
+		conv_size : [3, 3],
+		conv_stride : [1, 1]
+	},
+}
