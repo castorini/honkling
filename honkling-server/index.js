8711a4a0e9cb4902bc7b7bfeda1cb8dbc1deedc9
Brandon Lee <ljj7975@gmail.com>
2020-05-09 19:50:14 -0400

    working version of demo

---
+++ honkling-server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+var logger = require('./middleware/logger');
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/view/index.html'));
+});
+
+app.use(express.static(__dirname));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log('Server started on PORT ' + PORT));
